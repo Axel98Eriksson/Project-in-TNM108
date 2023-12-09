ec422a958b2a39dff0655fe2b257a97608901031
gustafalmesaker <114588941+gustafalmesaker@users.noreply.github.com>
2023-12-09 14:33:19 +0100

    lasso and ridge tested

--- code/CA-prediction.py
+++ code/CA-prediction.py
@@ -1,17 +1,42 @@
 import joblib
-from matplotlib.pyplot import plot
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class GaussianFeatures(BaseEstimator, TransformerMixin):
+    """Uniformly spaced Gaussian features for one-dimensional input"""
+    def __init__(self, N, width_factor=2.0):
+        self.N = N
+        self.width_factor = width_factor
+        
+    @staticmethod
+    def _gauss_basis(x, y, width, axis=None):
+        arg = (x - y) / width
+        return np.exp(-0.5 * np.sum(arg ** 2, axis))
+
+    def fit(self, X, y=None):
+        # create N centers spread along the data range
+        self.centers_ = np.linspace(X.min(), X.max(), self.N)
+        self.width_ = self.width_factor * (self.centers_[1] - self.centers_[0])
+        return self
+    
+    def transform(self, X):
+        return self._gauss_basis(X[:, :, np.newaxis], self.centers_, self.width_, axis=1)
+
+gauss_model = make_pipeline(GaussianFeatures(20),LinearRegression())
+gauss_model.fit(x[:, np.newaxis], y)
+yfit = gauss_model.predict(xfit[:, np.newaxis])
 
 
 # Load HTML data
-data = pd.read_html(r'data\train\train-model-v0.html', header=0, encoding='utf-8', keep_default_na=False)
+data = pd.read_html(r'data\train\strikers.html', header=0, encoding='utf-8', keep_default_na=False)
 
 # Assuming the first table in the HTML file is the one you want to use
 # If there are multiple tables, you may need to inspect and select the correct one
@@ -29,11 +54,18 @@ labelEncoder = LabelEncoder()
 labelEncoder.fit(data['Position'])
 data['Position'] = labelEncoder.transform(data['Position'])
 
-labelEncoder.fit(data['Right Foot'])
-data['Right Foot'] = labelEncoder.transform(data['Right Foot'])
+#RIGHT/LEFT FOOT SYSTEM
+data['Left Foot'] = data['Left Foot'].replace({'Very Strong' : '6','Strong' : '5', 'Fairly Strong' : '4', 'Reasonable' : '3', 'Weak' : '2', 'Very Weak' : '1'})
+data['Right Foot'] = data['Right Foot'].replace({'Very Strong' : '6','Strong' : '5', 'Fairly Strong' : '4', 'Reasonable' : '3', 'Weak' : '2', 'Very Weak' : '1'})
 
-labelEncoder.fit(data['Left Foot'])
-data['Left Foot'] = labelEncoder.transform(data['Left Foot'])
+
+# Convert the columns to numeric
+data['Left Foot'] = pd.to_numeric(data['Left Foot'])
+data['Right Foot'] = pd.to_numeric(data['Right Foot'])
+
+data['Weakfoot Score'] = data['Left Foot'] + data['Right Foot']
+data = data.drop(['Left Foot','Right Foot'], axis=1)
+
 
 y = data['CA']
 X = data.drop('CA', axis=1)
@@ -43,30 +75,70 @@ data_scaled = scaler.fit_transform(X)
 
 X_train, X_test, y_train, y_test = train_test_split(data_scaled, y, test_size=0.75, random_state=42)
 
-# Create and fit the linear regression model
-model = LinearRegression(fit_intercept=True)
-model.fit(X_train, y_train)
+# Linear Regression
+linear_model = LinearRegression(fit_intercept=True)
+linear_model.fit(X_train, y_train)
+y_pred_linear = linear_model.predict(X_test)
 
-# Make predictions on the test set
-y_pred = model.predict(X_test)
+# Ridge Regression
+#ridge_model = Ridge(alpha=1.0)  # You can experiment with different alpha values
+ridge_model = make_pipeline(GaussianFeatures(30), Ridge(alpha=0.1))
+ridge_model.fit(X_train, y_train)
+y_pred_ridge = ridge_model.predict(X_test)
 
-#evaluate the model
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
-#accuracy = accuracy_score(y_test, y_pred)
+# Lasso Regression
+#lasso_model = Lasso(alpha=1.0)  # You can experiment with different alpha values
+lasso_model = make_pipeline(GaussianFeatures(30), Lasso(alpha=0.001))
+lasso_model.fit(X_train, y_train)
+y_pred_lasso = lasso_model.predict(X_test)
 
-print(f'Mean Squared Error: {mse}')
-print(f'R-squared: {r2}')
-#print(f'Accuracy: {accuracy}')
+# Evaluate Linear Regression
+mse_linear = mean_squared_error(y_test, y_pred_linear)
+r2_linear = r2_score(y_test, y_pred_linear)
+print(f'Linear Regression - Mean Squared Error: {mse_linear}')
+print(f'Linear Regression - R-squared: {r2_linear}')
 
-# Scatter plot of actual vs. predicted values
-line = np.linspace(0,200)
-plt.plot(line,line, color = 'red')
-plt.scatter(y_test, y_pred, alpha=0.2, color="white", edgecolors="blue")
-plt.title('Actual vs. Predicted Current Ability')
+# Evaluate Ridge Regression
+mse_ridge = mean_squared_error(y_test, y_pred_ridge)
+r2_ridge = r2_score(y_test, y_pred_ridge)
+print(f'Ridge Regression - Mean Squared Error: {mse_ridge}')
+print(f'Ridge Regression - R-squared: {r2_ridge}')
+
+# Evaluate Lasso Regression
+mse_lasso = mean_squared_error(y_test, y_pred_lasso)
+r2_lasso = r2_score(y_test, y_pred_lasso)
+print(f'Lasso Regression - Mean Squared Error: {mse_lasso}')
+print(f'Lasso Regression - R-squared: {r2_lasso}')
+
+# Scatter plot of actual vs. predicted values for Linear Regression
+line = np.linspace(0, 200)
+plt.plot(line, line, color='red')
+plt.scatter(y_test, y_pred_linear, alpha=0.2, color="white", edgecolors="blue", label='Linear Regression')
+plt.title('Actual vs. Predicted Current Ability - Linear Regression')
 plt.xlabel('Actual CA')
 plt.ylabel('Predicted CA')
+plt.legend()
 plt.show()
 
-#save the model for experimentation
-joblib.dump(model, r'data\models\v02.joblib')
+# Scatter plot of actual vs. predicted values for Ridge Regression
+plt.plot(line, line, color='red')
+plt.scatter(y_test, y_pred_ridge, alpha=0.2, color="white", edgecolors="green", label='Ridge Regression')
+plt.title('Actual vs. Predicted Current Ability - Ridge Regression')
+plt.xlabel('Actual CA')
+plt.ylabel('Predicted CA')
+plt.legend()
+plt.show()
+
+# Scatter plot of actual vs. predicted values for Lasso Regression
+plt.plot(line, line, color='red')
+plt.scatter(y_test, y_pred_lasso, alpha=0.2, color="white", edgecolors="purple", label='Lasso Regression')
+plt.title('Actual vs. Predicted Current Ability - Lasso Regression')
+plt.xlabel('Actual CA')
+plt.ylabel('Predicted CA')
+plt.legend()
+plt.show()
+
+# Save the models for experimentation
+joblib.dump(linear_model, r'data\models\linear_regression.joblib')
+joblib.dump(ridge_model, r'data\models\ridge_regression.joblib')
+joblib.dump(lasso_model, r'data\models\lasso_regression.joblib')
--- code/gameplay.py
+++ code/gameplay.py
@@ -3,8 +3,8 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
 
-data = pd.read_html(r'data\season0\DC.html', header=0, encoding='utf-8', keep_default_na=False)
-v0 = joblib.load(r'data\models\v02.joblib')
+data = pd.read_html(r'data\season0\strikers.html', header=0, encoding='utf-8', keep_default_na=False)
+v0 = joblib.load(r'data\models\lasso_regression.joblib')
 data = data[0]
 
 player_names = data['Name']
@@ -14,10 +14,22 @@ data = data.drop(['Inf', 'Name', 'CA'], axis=1)
 labelEncoder = LabelEncoder()
 labelEncoder.fit(data['Position'])
 data['Position'] = labelEncoder.transform(data['Position'])
-labelEncoder.fit(data['Right Foot'])
-data['Right Foot'] = labelEncoder.transform(data['Right Foot'])
-labelEncoder.fit(data['Left Foot'])
-data['Left Foot'] = labelEncoder.transform(data['Left Foot'])
+
+#labelEncoder.fit(data['Right Foot'])
+#data['Right Foot'] = labelEncoder.transform(data['Right Foot'])
+#labelEncoder.fit(data['Left Foot'])
+#data['Left Foot'] = labelEncoder.transform(data['Left Foot'])
+
+#RIGHT/LEFT FOOT SYSTEM
+data['Left Foot'] = data['Left Foot'].replace({'Very Strong' : '6','Strong' : '5', 'Fairly Strong' : '4', 'Reasonable' : '3', 'Weak' : '2', 'Very Weak' : '1'})
+data['Right Foot'] = data['Right Foot'].replace({'Very Strong' : '6','Strong' : '5', 'Fairly Strong' : '4', 'Reasonable' : '3', 'Weak' : '2', 'Very Weak' : '1'})
+
+# Convert the columns to numeric
+data['Left Foot'] = pd.to_numeric(data['Left Foot'])
+data['Right Foot'] = pd.to_numeric(data['Right Foot'])
+
+data['Weakfoot Score'] = data['Left Foot'] + data['Right Foot']
+data = data.drop(['Left Foot','Right Foot'], axis=1)
 
 
 scaler = StandardScaler()
@@ -30,7 +42,7 @@ result = pd.concat([player_names, potential_signings], axis=1)
 result['Actual CA'] = CA
 result['CA diff'] = abs(result['Actual CA']-result['CA'])
 
-result_sorted = result.sort_values(by='CA diff', ascending=False)
+result_sorted = result.sort_values(by='Actual CA', ascending=False)
 
 # Display the top 5 rows
 top_targets = result_sorted.head(100)
