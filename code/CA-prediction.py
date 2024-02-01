68b4dc91e757770b965a19a2e4a012e06a873719
gustafalmesaker <114588941+gustafalmesaker@users.noreply.github.com>
2024-02-01 15:14:20 +0100

    penultimate

--- code/CA-prediction.py
+++ code/CA-prediction.py
@@ -1,45 +1,17 @@
-import joblib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
-from sklearn.base import BaseEstimator, TransformerMixin
-
-class GaussianFeatures(BaseEstimator, TransformerMixin):
-    """Uniformly spaced Gaussian features for one-dimensional input"""
-    def __init__(self, N, width_factor=2.0):
-        self.N = N
-        self.width_factor = width_factor
-        
-    @staticmethod
-    def _gauss_basis(x, y, width, axis=None):
-        arg = (x - y) / width
-        return np.exp(-0.5 * np.sum(arg ** 2, axis))
-
-    def fit(self, X, y=None):
-        # create N centers spread along the data range
-        self.centers_ = np.linspace(X.min(), X.max(), self.N)
-        self.width_ = self.width_factor * (self.centers_[1] - self.centers_[0])
-        return self
-    
-    def transform(self, X):
-        return self._gauss_basis(X[:, :, np.newaxis], self.centers_, self.width_, axis=1)
-
-#gauss_model = make_pipeline(GaussianFeatures(20),LinearRegression())
-#gauss_model.fit(x[:, np.newaxis], y)
-#yfit = gauss_model.predict(xfit[:, np.newaxis])
 
 
 # Load HTML data
-data = pd.read_html(r'data\train\sampdoria-s1.html', header=0, encoding='utf-8', keep_default_na=False)
+data = pd.read_html(r'data\train\attackers.html', header=0, encoding='utf-8', keep_default_na=False)
 
-# Assuming the first table in the HTML file is the one you want to use
-# If there are multiple tables, you may need to inspect and select the correct one
+
 data = data[0]
 
 # Separate player names for visualization
@@ -76,25 +48,6 @@ data_scaled = scaler.fit_transform(X)
 X_train, X_test, y_train, y_test = train_test_split(data_scaled, y, test_size=0.75, random_state=42)
 
 # Linear Regression
-#linear_model = LinearRegression(fit_intercept=True)
-#linear_model.fit(X_train, y_train)
-#y_pred_linear = linear_model.predict(X_test)
-
-
-# Ridge Regression
-#ridge_model = Ridge(alpha=1.0)  # You can experiment with different alpha values
-#ridge_model = make_pipeline(GaussianFeatures(20), Ridge(alpha=0.1))
-#ridge_model.fit(X_train, y_train)
-#y_pred_ridge = ridge_model.predict(X_test)
-
-# Lasso Regression
-#lasso_model = Lasso(alpha=1.0)  # You can experiment with different alpha values
-#lasso_model = make_pipeline(GaussianFeatures(20), Lasso(alpha=0.1))
-#lasso_model.fit(X_train, y_train)
-#y_pred_lasso = lasso_model.predict(X_test)
-
-
-# Linear Regression
 linear_model = LinearRegression(fit_intercept=True)
 linear_model.fit(X_train, y_train)
 y_pred_linear = linear_model.predict(X_test)
@@ -113,20 +66,26 @@ y_pred_lasso = lasso_model.predict(X_test)
 
 # Evaluate Linear Regression
 mse_linear = mean_squared_error(y_test, y_pred_linear)
+mae_linear = mean_absolute_error(y_test,y_pred_linear)
 r2_linear = r2_score(y_test, y_pred_linear)
 print(f'Linear Regression - Mean Squared Error: {mse_linear}')
 print(f'Linear Regression - R-squared: {r2_linear}')
+print(f'Linear Regression - Mean Absolute Error: {mae_linear}')
 
 # Evaluate Ridge Regression
 mse_ridge = mean_squared_error(y_test, y_pred_ridge)
+mae_ridge = mean_absolute_error(y_test,y_pred_ridge)
 r2_ridge = r2_score(y_test, y_pred_ridge)
-print(f'Ridge Regression - Mean Squared Error: {mse_ridge}')
+print(f'Ridge Regression - Mean Squared Error: {mse_linear}')
+print(f'Ridge Regression - Mean Absolute Error: {mae_ridge}')
 print(f'Ridge Regression - R-squared: {r2_ridge}')
 
 # Evaluate Lasso Regression
 mse_lasso = mean_squared_error(y_test, y_pred_lasso)
+mae_lasso = mean_absolute_error(y_test,y_pred_lasso)
 r2_lasso = r2_score(y_test, y_pred_lasso)
-print(f'Lasso Regression - Mean Squared Error: {mse_lasso}')
+print(f'Lasso Regression - Mean Squared Error: {mse_linear}')
+print(f'Lasso Regression - Mean Absolute Error: {mae_lasso}')
 print(f'Lasso Regression - R-squared: {r2_lasso}')
 
 # Scatter plot of actual vs. predicted values for Linear Regression
--- code/gameplay.py
+++ code/gameplay.py
@@ -15,10 +15,6 @@ labelEncoder = LabelEncoder()
 labelEncoder.fit(data['Position'])
 data['Position'] = labelEncoder.transform(data['Position'])
 
-#labelEncoder.fit(data['Right Foot'])
-#data['Right Foot'] = labelEncoder.transform(data['Right Foot'])
-#labelEncoder.fit(data['Left Foot'])
-#data['Left Foot'] = labelEncoder.transform(data['Left Foot'])
 
 #RIGHT/LEFT FOOT SYSTEM
 data['Left Foot'] = data['Left Foot'].replace({'Very Strong' : '6','Strong' : '5', 'Fairly Strong' : '4', 'Reasonable' : '3', 'Weak' : '2', 'Very Weak' : '1'})
@@ -48,15 +44,3 @@ result_sorted = result.sort_values(by='CA', ascending=False)
 # Display the top 5 rows
 top_targets = result_sorted.head(5)
 print(top_targets)
-
-
-#S4
-# Flavius Daniliuc 161.440013        133  28.440013
-#Alessandro Circati  162.908728        134  28.908728
-#Andrei Borza  155.257195        121  34.257195
-#Marco Bertini  153.842875        128  25.842875
-#Theo  158.056192        124  34.056192
-#Adil Aouchiche  159.733716        126  33.733716
-#Yan Couto  156.540944        141  15.540944
-#Lucas Beltrán  162.104786        135  27.104786
-#Nikola Čavlina  173.626357        131  42.626357
